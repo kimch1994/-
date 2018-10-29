70bae8d01f7bf472731c0e9c6b2f04f4cdafe3c0
kimch1994 <43035427+kimch1994@users.noreply.github.com>
2018-10-30 03:29:20 +0900

    Add files via upload

---
+++ member.java
@@ -0,0 +1,33 @@
+package project;
+
+public class member {
+	private String email;
+	private String password;
+	private String password_check;
+	private String phone;
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getPassword_check() {
+		return password_check;
+	}
+	public void setPassword_check(String password_check) {
+		this.password_check = password_check;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+}
